c7fc2125b73ab66770bd0e03dbafe28c060de76d
csrissettled <101882657+csrissettled@users.noreply.github.com>
2022-09-13 15:54:41 -0400

    Update partialFunction.js

--- JS-Basics/partialFunction.js
+++ JS-Basics/partialFunction.js
@@ -18,3 +18,15 @@ user.sayNow = partial(user.say, new Date().getHours() + ':' + new Date().getMinu
 user.sayNow("Hello");
 // Something like:
 // [10:00] John: Hello!
+
+
+//--------------------using Bind---------------
+function mul(a, b) {
+  return a * b;
+}
+
+let triple = mul.bind(null, 3);
+
+console.log( triple(3) ); // = mul(3, 3) = 9
+console.log( triple(4) ); // = mul(3, 4) = 12
+console.log( triple(5) ); // = mul(3, 5) = 15
