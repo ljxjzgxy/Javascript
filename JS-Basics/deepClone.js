e6a8e20df02b28f189b6f5927df98b13de60e4a0
James <32525857+ljxjzgxy@users.noreply.github.com>
2024-01-29 10:11:30 -0500

    Update deepClone.js

--- JS-Basics/deepClone.js
+++ JS-Basics/deepClone.js
@@ -1,15 +1,32 @@
 //refer to https://youtu.be/4Ej0LwjCDZQ?t=1078
-const deepClone =(obj)=>{
-  if(typeof obj !=="object" || obj ===null) return obj;
+// const deepClone =(obj)=>{
+//   if(typeof obj !=="object" || obj ===null) return obj;
   
-  const newObject = Array.isArray(obj)? [] : {};
+//   const newObject = Array.isArray(obj)? [] : {};
   
-  for(const key in obj){
-    const value = obj[key];    
-    newObject[key] = deepClone(value);
+//   for(const key in obj){
+//     const value = obj[key];    
+//     newObject[key] = deepClone(value);
+//   }
+//   return newObject;
+// }
+
+//https://juejin.cn/post/7325040809697591296
+function deepClone(obj) {
+  if (obj === null || typeof obj !== 'object') {
+    return obj;
   }
-  return newObject;
-}
+
+  const clonedObj = Array.isArray(obj) ? [] : {};
+
+  for (let key in obj) {
+    if (obj.hasOwnProperty(key)) {
+      clonedObj[key] = deepClone(obj[key]);
+    }
+  }
+
+  return clonedObj;
+} 
 
 
 //solution two, https://javascript.info/prototype-methods
