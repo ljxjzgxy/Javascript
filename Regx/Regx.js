222621fca3249e48917f6aa7f28cd10a90c1fec0
James <32525857+ljxjzgxy@users.noreply.github.com>
2024-04-23 08:53:12 -0400

    add thousandSeparator

--- Regx/Regx.js
+++ Regx/Regx.js
@@ -3,3 +3,12 @@
 function isEmail(email) {
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
+
+
+//regx thousand comma separator
+function thousandSeparator(num,separator = ',') {
+	const reg = /(?=(\B)(\d{3})+$)/g;
+	return num.replace(reg,separator);	 
+}
+ 
+
