320d4f93257308b9dd71bd64f1ceea6253f358ac
crisissettled <101882657+crisissettled@users.noreply.github.com>
2024-04-23 09:20:56 -0400

    another email regx

--- Regx/Regx.js
+++ Regx/Regx.js
@@ -1,6 +1,9 @@
 
 //https://codepen.io/FlorinPop17/pen/OJJKQeK
 function isEmail(email) {
+	/*another one
+	 const reg = /\b[\w.%+-]+@[\w.-]+\.[A-z]{2,6}\b/
+        */
 	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
 
