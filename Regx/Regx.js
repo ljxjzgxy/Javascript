443eb73d0a9edb5c6cab8e717c8d9cba4e8a0b2d
crisissettled <101882657+crisissettled@users.noreply.github.com>
2024-04-23 12:03:12 -0400

    add float support for thoudsands comma separator

--- Regx/Regx.js
+++ Regx/Regx.js
@@ -10,8 +10,20 @@ function isEmail(email) {
 
 //regx thousand comma separator
 function thousandSeparator(num,separator = ',') {
-	const reg = /(?=(\B)(\d{3})+$)/g;
-	return num.replace(reg,separator);	 
+	const regInteger = /(?=(\B)(\d{3})+$)/g;
+        const regFloat = /(?=(\B)((\d{3})+(\.\d*){1}$))/g;
+	let reg = null
+	if (Number(item) === item) { 
+	    if(item % 1 === 0){ //integer
+	      	reg = regInteger;
+	    } else { // float
+		reg = regFloat;
+	    }
+	}
+
+	if(reg === null) return num;
+	
+	return num.toString().replace(reg,separator);	 
 }
  
 
