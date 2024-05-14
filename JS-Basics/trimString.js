e52780982440a6e16a22b6f145638ce184eac8d1
crisissettled <101882657+crisissettled@users.noreply.github.com>
2024-05-14 10:33:17 -0400

    Update trimString.js

--- JS-Basics/trimString.js
+++ JS-Basics/trimString.js
@@ -1,5 +1,5 @@
 // trim given characters for both ends of a string 
-function trim(strValue, charToTrim){  
+function trimSpecial(strValue, charToTrim){  
   if(!strValue) return "";
   const pattern  = new RegExp(`^\\${charToTrim}+|\\${charToTrim}+$`,"g");
 
